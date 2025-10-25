0f0cadcd8cb25e701504e24bc66676bfd0df3efd
Igor <igor.prahin@gmail.com>
2025-10-25 19:25:09 +0700

    final tests

--- tests/models/LedgerEntry.test.ts
+++ tests/models/LedgerEntry.test.ts
@@ -1,17 +1,34 @@
-import { LedgerEntry } from "../../src/models";
+import { LedgerEntry, User, Order } from "../../src/models";
 import { LedgerEntryType } from "../../src/models/LedgerEntry";
 import { sequelize } from "../../src/config/database";
 import { fn, col, literal } from 'sequelize';
+import { OrderStatus } from "../../src/models/Order";
+import { UserStatus } from "../../src/models/User";
 
 describe('LedgerEntry Balance Tests', () => { 
     beforeAll(async () => {
         await sequelize.sync({ force: true });
         
-        // Создаём тестовые записи для транзакции 1
-        await LedgerEntry.bulkCreate([
-        { userId: 1, orderId: 1, type: LedgerEntryType.DEBIT, amount: 100, description: "Order #1" },
-        { userId: 1, orderId: 1, type: LedgerEntryType.CREDIT, amount: 100, description: "Order #1"  }
-        ]);
+ await User.create({
+      email: 'testuser@gmail.com',
+      balance: 1000,
+      status: UserStatus.ACTIVE
+    });
+
+    await Order.create({
+      userId: 1,
+      amount: 100,
+      status: OrderStatus.COMPLETED,
+      idempotencyKey: 'test-order-1'
+    });
+
+    await LedgerEntry.bulkCreate([
+      { userId: 1, orderId: 1, type: LedgerEntryType.DEBIT, amount: 100, description: "Order #1" },
+      { userId: 1, orderId: 1, type: LedgerEntryType.CREDIT, amount: 100, description: "Order #1" }
+    ]);
+
+
+
     });
 
 
